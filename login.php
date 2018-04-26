0606a3a6aae374431ed2f4710dfba25c83c7366e
pooldiver69 <38307992+pooldiver69@users.noreply.github.com>
2018-04-25 17:45:12 -0700

    Update login.php

--- login.php
+++ login.php
@@ -22,7 +22,7 @@ else { // User exists
         // This is how we'll know the user is logged in
         $_SESSION['logged_in'] = true;
 
-        header("location: sell&buy.html");
+        header("location: buy&sell.html");
     }
     else {
         $_SESSION['message'] = "You have entered wrong password, try again!";
